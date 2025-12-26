b9fe93160944831407b0bde3488eb71dc96b7779
JGabrielRS <joaogabrielrsousa07@gmail.com>
2025-12-26 17:23:10 -0300

    tab

--- Gauss/Gauss.cpp
+++ Gauss/Gauss.cpp
@@ -13,12 +13,12 @@ void Gauss::iterar(){
     for (int i=0; i < n-1; i++){
         for (int k=i+1; k < n; k++){
             if (m[i][i]!=0){
-            float x = -m[k][i]/m[i][i];
-            m[k][i] = 0;
-            for (int j=i+1; j < n; j++){
-                m[k][j] = m[k][j] + x*m[i][j];
-            }
-            b[k] = b[k] + x*b[i];
+                float x = -m[k][i]/m[i][i];
+                m[k][i] = 0;
+                for (int j=i+1; j < n; j++){
+                    m[k][j] = m[k][j] + x*m[i][j];
+                }
+                b[k] = b[k] + x*b[i];
             } else {
                 return;
             }
