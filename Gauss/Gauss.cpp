a1e27ac5d9568990ccda51412341107af050a395
Eric <eric.rodrig.gomes@gmail.com>
2025-12-26 18:11:51 -0300

    Generalização do Gauss

--- Gauss/Gauss.cpp
+++ Gauss/Gauss.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
 #include "Gauss.h"
+#include "../Matriz/matriz.h"
 
-Gauss::Gauss(std::vector<std::vector<float>> m, std::vector<float> b)
-: m(m), b(b), solucao(m.size()) {}
-
-std::vector<float> Gauss::get_solucao(){
-    return solucao;
-}
+Gauss::Gauss(Matriz m, std::vector<float> b)
+: m(m), b(b)) {}
 
 void Gauss::iterar(){
     int n = m.size();
     for (int i=0; i < n-1; i++){
+        // pivotação
+        int maior = i;
+        for(int j = i + 1; j < n; ++j){
+            if (abs(m[i][j]) > abs(m[i][maior])) maior = j;
+        }
+
         for (int k=i+1; k < n; k++){
-            if (m[i][i]!=0){
-                float x = -m[k][i]/m[i][i];
-                m[k][i] = 0;
-                for (int j=i+1; j < n; j++){
-                    m[k][j] = m[k][j] + x*m[i][j];
-                }
-                b[k] = b[k] + x*b[i];
-            } else {
-                return;
-            }
+            float x = -m[k][i]/m[i][i];
+            m[k][i] = 0;
+
+            // iteracao respectiva
+            iterar(i, x);
+            b[k] = b[k] + x*b[i];
         }
     }
 }
