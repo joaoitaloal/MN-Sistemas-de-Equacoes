d2308b712360e266c207cdc547a2083fa41085ec
Eric <eric.rodrig.gomes@gmail.com>
2025-12-26 21:30:30 -0300

    GaussJordan quase certo

--- src/Gauss/GaussNomal.cpp
+++ src/Gauss/GaussNomal.cpp
@@ -9,8 +9,8 @@ void GaussNormal::iterar(int i){
         double x = -mat.at(k, i)/mat.at(i, i);
         mat.set(k, i, 0);
 
-        for (int j = i; j < n; j++) {
-            mat.set(k, j, mat.at(k, j) - (x * mat.at(i, j)));
+        for (int j = i + 1; j < n; j++) {
+            mat.set(k, j, mat.at(k, j) + (x * mat.at(i, j)));
         }
             
         b.set(k, 0, b.at(k, 0) + x * b.at(i, 0));
