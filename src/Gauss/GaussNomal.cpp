5fdc800eefc985283bfc35bfd6794d0380527962
João Ítalo <161601649+joaoitaloal@users.noreply.github.com>
2025-12-26 21:23:08 -0300

    Consertando problemas

--- src/Gauss/GaussNomal.cpp
+++ src/Gauss/GaussNomal.cpp
@@ -5,11 +5,10 @@ void GaussNormal::iterar(int i){
     int n = mat.get_size().first;
 
     for (int k = i+1; k < n; k++){
-        if (k == i) continue;
         double x = -mat.at(k, i)/mat.at(i, i);
         mat.set(k, i, 0);
 
-        for (int j = i; j < n; j++) {
+        for (int j = k; j < n; j++) {
             mat.set(k, j, mat.at(k, j) - (x * mat.at(i, j)));
         }
             
