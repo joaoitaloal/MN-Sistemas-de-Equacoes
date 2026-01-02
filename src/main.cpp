c3e46cadfb92afd77699a2d5293db28447ccd691
Guilherme Gondim <guilhermeglga@alu.ufc.br>
2026-01-02 17:15:35 -0300

    Adicionando template do main final

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,12 @@
 #include "Gauss/GaussJordan.h"
 
 int main(){
-    Matrix A({
-        {10,  1,  1},
-        {1,  10,  1},
-        {1,  1,  10}
-    });
-    Matrix B({
-        {12},
-        {12},
-        {12}
-    });
-    
-    cout << "MATRIZES INICIAIS" << endl << endl;
-    cout << "A:" << endl;
-    A.print();
-    cout << endl;
-    cout << "B:" << endl;
-    B.print();
-    cout << "----------------" << endl;
-    Gauss gauss_normal(A, B);
-    double det_normal = gauss_normal.eliminar_gauss();
-    cout << "MATRIZES APÓS A APLICAÇÃO DO MÉTODO DE GAUSS" << endl << endl;
-    cout << "A:" << endl;
-    gauss_normal.print_mat();
-    cout << endl;
-    cout << "B:" << endl;
-    gauss_normal.print_b();
-    cout << endl;
-    cout << "DETERMINANTE: " << det_normal << endl;
-    cout << "----------------" << endl;
-    GaussJordan gauss_jordan(A, B);
-    double det_jordan = gauss_jordan.eliminar_gauss();
-    cout << "MATRIZES APÓS A APLICAÇÃO DO MÉTODO DE GAUSS-JORDAN" << endl << endl;
-    cout << "A:" << endl;
-    gauss_jordan.print_mat();
-    cout << endl;
-    cout << "B:" << endl;
-    gauss_jordan.print_b();
-    cout << endl;
-    cout << "DETERMINANTE: " << det_jordan << endl;
+    // Entrada
+
+    // Processamento
+
+
+    // Saída
 
     return 0;
 }
